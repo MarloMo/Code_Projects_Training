cd06dfd0b703cb6cdb11514686e2c291a0172e35
Marlo Morales <moralesmarlo486@gmail.com>
2024-06-20 17:16:59 -0700

    update searching arrays training

--- online_training/codecademy/Pass_Tech_Interviews_Python_Intro/Data_Structures/Linear_Data_Structures/Searching_Arrays/format.py
+++ online_training/codecademy/Pass_Tech_Interviews_Python_Intro/Data_Structures/Linear_Data_Structures/Searching_Arrays/format.py
@@ -1,20 +1,44 @@
-def binary_search(sorted_list, target):
-    if not sorted_list:
-        return 'value not found'
+def sparse_search(data, search_val):
+    print("Data: " + str(data))
+    print("Search Value: " + str(search_val))
 
-    mid_idx = len(sorted_list) // 2
-    mid_val = sorted_list[mid_idx]
+    first = 0
+    last = len(data) - 1  # 10
 
-    if mid_val == target:
-        return mid_idx
-    elif mid_val > target:
-        left_half = sorted_list[:mid_idx]
-        return binary_search(left_half, target)
-    elif mid_val < target:
-        right_half = sorted_list[mid_idx + 1:]
-        result = binary_search(right_half, target)
+    while first <= last:
+        mid = (first + last) // 2  # 5, 4
+        print(mid)
 
-        if result == "value not found":
-            return result
+        # Move mid to the closest non-empty value
+        if not data[mid]:
+            left = mid - 1  # 4,
+            right = mid + 1  # 6,
 
-        return result + mid_idx + 1
+            while True:
+                if left < first and right > last:
+                    return f"{search_val} is not in the dataset"
+                elif left >= first and data[left]:
+                    mid = left
+                    break
+                elif right <= last and data[right]:
+                    mid = right
+                    break
+
+                left -= 1
+                right += 1
+
+        # Perform the check after finding a non-empty value
+        if data[mid] == search_val:
+            return f"{search_val} found at position {mid}"
+        # If data[mid] < search_val, it means the value at the middle index is
+        # alphabetically less than the search value. For example, if mid points
+        # to "Art" and search_val is "Ball", since "Art" comes before "Ball"
+        # alphabetically, the search needs to continue in the right half of the list.
+        # This is because the list is sorted, and all values after "Art" could potentially
+        # be "Ball" or come alphabetically after "Art".
+        elif data[mid] < search_val:
+            first = mid + 1
+        else:
+            last = mid - 1
+
+    return f"{search_val} is not in the dataset"
