66d374f6b2d792d556bdde72a3cbb551da30207c
Marlo Morales <moralesmarlo486@gmail.com>
2024-07-17 17:31:10 -0700

    update heaps code and add bubble sort notebook

--- online_training/codecademy/Pass_Tech_Interviews_Python_Intro/Nonlinear_Data_Structures/Heaps/cc_heaps.py
+++ online_training/codecademy/Pass_Tech_Interviews_Python_Intro/Nonlinear_Data_Structures/Heaps/cc_heaps.py
@@ -11,6 +11,25 @@ random_nums = [randrange(1, 101) for n in range(6)]
 for el in random_nums:
     min_heap.add(el)
 
+min_heap.add(1)
 # remove minimum until min_heap is empty
-while min_heap.count != 0:
-    min_heap.retrieve_min()
+# while min_heap.count != 0:
+#     min_heap.retrieve_min()
+
+
+### TESTING GROUNDS
+my_min_heap = MinHeap()
+my_min_heap.add(10)
+my_min_heap.add(13)
+my_min_heap.add(21)
+my_min_heap.add(61)
+my_min_heap.add(22)
+my_min_heap.add(23)
+my_min_heap.add(99)
+print("My Min Heap List: \n", my_min_heap.heap_list)
+# my_min_heap.add(12)
+# my_min_heap.add(1)
+
+while len(my_min_heap.heap_list) != 1:
+    print(my_min_heap.heap_list)
+    my_min_heap.retrieve_min()
